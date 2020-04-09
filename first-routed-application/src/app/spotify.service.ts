d0632b4787385a5ef0040f34d9fd100d7ab2e2e9
Marcoooo01 <grossi.marco@iisgalvanimi.edu.it>
2020-04-09 16:03:30 +0000

    01.1

--- first-routed-application/src/app/spotify.service.ts
+++ first-routed-application/src/app/spotify.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
+const headers = new HttpHeaders({Authorization: environment.oauthToken});
 //Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
   providedIn: 'root'
@@ -12,13 +14,13 @@ export class SpotifyService {
 
   searchTrack(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
-    const headers = new HttpHeaders({
-      Authorization:
-        'Bearer BQCzzSVivq2j0VMJ525eZP5Y-qvM4R6WP3j8wMROmstp6PISCQgfjrlu3ZqW_Wy2aLWwWAFVl9nGtZO8OYggceBHmIHRS-GPO9o_y5k6WfEe0FiJljNm1Mb9zVo1XieE_u4GNZEbZcY'
-    });
-
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
  //Ritorno un observable ai componenti che richiedono il servizio
+  }
+
+  getTrack(id: string) {
+    const url = `https://api.spotify.com/v1/tracks/${id}`;
+    return this.http.get(url, { headers });
   }
 }
--- first-routed-application/src/environments/environment.ts
+++ first-routed-application/src/environments/environment.ts
@@ -1,16 +1,4 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  oauthToken: "Bearer BQBvrd4AosoTa7GKqqOdZBQndb71AA6DIU-rVurYmVKbUeiqyOT_k7f0a1i84Yu57Lz3qdoFE14WEaYmvQ4A7w-Y769oP6Tvd_C48WvjDT8TmTM0rFoHj10XY_BKrHPLBJMOPnT29s0"
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
